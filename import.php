1f7a3f19affbdba17884f920391fedf22069d4cb
tokens <pablorotem6@gmail.com>
2024-10-14 17:50:23 +0300

    Update import.php

--- import.php
+++ import.php
@@ -5,6 +5,7 @@ session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+// do composer installation first - composer require phpoffice/phpspreadsheet
 
 // Database connection
 try {
